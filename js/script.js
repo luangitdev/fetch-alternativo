085ef3e1661265244b2946e265cb546d78e4dd02
luangitdev <luaninveste@gmail.com>
2023-08-20 16:59:41 -0300

    Inclusão do index e script

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//Reescrevendo o fetch separando tudo por função.
+async function loadPosts(){
+    let req = await fetch('https://jsonplaceholder.typicode.com/posts');
+    return req.json();
+}
+
+function changeToLoadingState(){
+    document.getElementById("posts").innerHTML = "Carregando....";
+}
+
+function renderPost(post){
+    return `
+        <h3>${post.title}</h3>
+        ${post.body}<br/>
+        <hr/>
+    `;
+}
+
+function renderPosts(posts){
+    return posts.map((post) => renderPost(post)).join("");
+}
+
+async function fetchPostsAndRender(){
+    changeToLoadingState();
+    const posts = await loadPosts();
+    document.getElementById("posts").innerHTML = renderPosts(posts);
+}
